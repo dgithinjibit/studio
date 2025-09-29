964079c43f19238dfb26202c95491c341cde7b8b
Daniel Githinji <dgithinji331@gmail.com>
2025-09-29 07:50:00 +0000

    lets use the colours for our whole website to those in this css file, do

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,7 @@ export default function RootLayout({
         <link rel="icon" href="/logo.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
         {children}
