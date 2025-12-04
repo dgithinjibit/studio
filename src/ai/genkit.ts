b6204079e50452f3f7ed884e92ccc67681d94af2
Daniel Githinji <dgithinji331@gmail.com>
2025-12-04 08:51:48 +0000

    hey there is no vertex right now, it was just an idea, lets use what we

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,9 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/google-genai';
-import {vertexAI} from '@genkit-ai/google-genai/vertexai';
+import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    vertexAI()
   ],
-  model: 'vertexai/gemini-1.5-flash-preview-0514',
+  model: 'googleai/gemini-1.5-flash',
 });
