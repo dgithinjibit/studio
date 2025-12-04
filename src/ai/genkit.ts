85cf19cde1cf2b260eec91fe05b1bf8ab14d8f33
Daniel Githinji <dgithinji331@gmail.com>
2025-12-04 12:53:44 +0000

    , delete any irrelevant code to here which tyou had ealer thouhgt is you

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,10 @@
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {vertexAI} from '@genkit-ai/googleai/vertexai';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    vertexAI(),
   ],
-  model: 'vertexai/gemini-1.5-flash-preview-0514',
+  model: 'googleai/gemini-pro',
 });
