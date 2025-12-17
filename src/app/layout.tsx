b75553d48db992986a6113c54755aa05e5391497
Daniel Githinji <dgithinji331@gmail.com>
2025-12-17 07:23:16 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
-
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import './globals.css';
-import { Toaster } from "@/components/ui/toaster"
 import { app } from '@/lib/firebase'; // Ensure Firebase is initialized
-import { ThemeProvider } from '@/components/theme-provider';
-import { ThemeToggleButton } from '@/components/ui/theme-toggle-button';
 import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import { MetaMaskFix } from '@/components/metamask-fix';
 import Script from 'next/script';
+import { ClientProviders } from '@/components/client-providers';
 
 const fontSans = Inter({
   subsets: ["latin"],
@@ -34,16 +31,9 @@ export default function RootLayout({
         <MetaMaskFix />
       </head>
       <body className={cn("font-body antialiased", fontSans.variable)}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ClientProviders>
           {children}
-          <Toaster />
-          <ThemeToggleButton />
-        </ThemeProvider>
+        </ClientProviders>
         <Script src='https://meet.jit.si/external_api.js' async />
       </body>
     </html>
