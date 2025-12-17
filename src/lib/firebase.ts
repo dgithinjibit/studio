929e1303545b39e91b4dd08eb5690b1baa448a31
Daniel Githinji <dgithinji331@gmail.com>
2025-12-17 07:35:48 +0000

    here are theneeded details from my firebase console // Import the functi

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "educloud-kenya.firebaseapp.com",
-  projectId: "educloud-kenya",
-  storageBucket: "educloud-kenya.appspot.com",
-  messagingSenderId: "1083742475824",
-  appId: "1:1083742475824:web:a69902b69297ad4dea3ff5",
-  measurementId: "G-40Q2Q2149K"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
