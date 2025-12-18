6125e8d1b5d9369dd9d21377880ec75acf0af368
Daniel Githinji <dgithinji331@gmail.com>
2025-12-18 07:25:29 +0000

    okay and what of the last part,, any chagnes  we neee?

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({apiKey: process.env.GEMINI_API_KEY || 'AIzaSyB3PyfAfinamByQHfoVuRmQMHYg1Mzqgyc'}),
+    googleAI({apiKey: process.env.GEMINI_API_KEY}),
   ],
   model: 'googleai/gemini-2.0-flash',
 });
