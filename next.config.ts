65c78dbb36784e899bb8227c71a0e35900f5bc95
Daniel Githinji <dgithinji331@gmail.com>
2026-02-28 07:59:07 +0000

    The error message is explicit: **Your version of Next.js does not suppor

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,2 @@
-
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-       {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
-    ],
-  },
-};
-
-export default nextConfig;
+// This file is unused. Please use next.config.js instead for Next.js 14 compatibility.
+module.exports = {};
